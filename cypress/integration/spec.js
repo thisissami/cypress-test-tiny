f626ba5edcbebf12eafcb98dae7851bc8496d28c
thisissami <sami@rubikon.io>
2020-06-10 11:46:16 -0700

    new expect bug

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,30 +1,12 @@
 /// <reference types="cypress" />
 
-const clickAntDesignSelectOption = option => {
-  cy.get('.ant-select-dropdown')
-    // replace the regular expression with the string to remove one of the bugs.
-    // .contains(option)
-    .contains(new RegExp(option, 'g'))
-    .wait(2000)
-    .click()
-    .wait(2000);
-};
-
 describe('page', () => {
   it('works', () => {
     cy.visit('/');
 
-    cy.contains('Toggle Modal Visibility')
-      .click();
-
-    cy.findByLabelText('One or two?')
-      .click();
-
-    clickAntDesignSelectOption('One');
-
-    cy.findByLabelText('Three or four?')
-      .click();
-
-    clickAntDesignSelectOption('Three');
+    expect({
+      v1: 1,
+      v2: 2
+    }).to.have.key('v1');
   });
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,7 @@
-import React, { useState } from 'react';
-import ModalForm from './ModalForm';
+import React from 'react';
 
-export default () => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-
-  const toggleModalVisibility = () => { setModalIsOpen(!modalIsOpen) };
-
-  return (
-    <main>
-      <h1>Ruh-roh! It's a Cypress bug!</h1>
-      <ModalForm modalIsOpen={modalIsOpen} closeModal={toggleModalVisibility} />
-
-      <button onClick={toggleModalVisibility}>Toggle Modal Visibility</button>
-
-      {/* Comment out the rest of this component to test without the background jumping.
-        It's also buggy! The click is not centered on the `div` that should be clicked! */}
-
-      <br /><br />
-      {
-        Array(42).fill(1).map((_, i) => (
-          <h2 key={i}>Helping make the Y dimension long! {i+1}</h2>
-        ))
-      }
-    </main>
-  );
-};
+export default () => (
+  <main>
+    <h1>Expect Bug</h1>
+  </main>
+);
